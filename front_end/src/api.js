bc24af0b38d1aa1655c81077f514c24f8453165a
An Yu <44589264+CrazyGeG@users.noreply.github.com>
2025-04-15 15:30:37 -0400

    Update api.js

--- front_end/src/api.js
+++ front_end/src/api.js
@@ -2,14 +2,15 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://3.136.25.8:9000', 
-  });
+  baseURL: 'http://3.136.25.8:9000', 
+});
 
-// API Functions
-export const getStudents = () => api.get('/students');
-export const addStudent = (student) => api.post('/students', student);
-export const updateStudent = (id, student) => api.put(`/students/${id}`, student);
-export const patchStudent = (id, partialData) => api.patch(`/students/${id}`, partialData);
-export const deleteStudent = (id) => api.delete(`/students/${id}`);
+// API Functions (prepend /api to all routes)
+export const getStudents = () => api.get('/api/students');
+export const addStudent = (student) => api.post('/api/students', student);
+export const updateStudent = (id, student) => api.put(`/api/students/${id}`, student);
+export const patchStudent = (id, partialData) => api.patch(`/api/students/${id}`, partialData);
+export const deleteStudent = (id) => api.delete(`/api/students/${id}`);
 
 export default api;
+
